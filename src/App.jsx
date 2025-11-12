86abbaa47649231f4f08ec0c10ff45caf45868a7
Dinesh Thangavel <dineshthangavel14@gmail.com>
2025-11-12 19:28:18 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-import { Route,Router,Routes} from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import User from './components/User.jsx';
 function App() {
     const [stories,setStories]=useState(false);
     return(
-      <Router basename='/kadhaipoma'>
+      <Router >
       <Routes>
          <Route path='/' element={
         <User stories={stories} setStories={setStories} /> }/>
